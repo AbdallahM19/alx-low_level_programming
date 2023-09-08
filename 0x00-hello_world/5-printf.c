922884033966f4cedbb4e6229e2c75aca4034fa1
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-07 18:07:00 -0700

    first program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
@@ -8,7 +8,8 @@
  * Return: Always 0 (Success)
 */
 
-int main(void) {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
