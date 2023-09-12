48d68744f89d7c3755f1fae72bd29d4e707ddcbe
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-12 09:00:14 -0700

    Functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,12 @@
 
 void print_alphabet_x10(void)
 {
-int char1, line;
-for (line = 0; line <= 9; line++)
-{
-for (char1 = 'a'; char1 <= 'z'; char1++)
-{
-_putchar(char1);
-}
-_putchar('\n');
-}
-return (0);
+	int line, char1;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (char1 = 'a'; char1 <= 'z'; char1++)
+			_putchar(char1);
+		_putchar('\n');
+	}
 }
