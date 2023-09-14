78af2a657fc7aa10a06cd428fad956411fe7ed28
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-14 08:53:51 -0700

    More_Functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,23 +3,24 @@
 /**
  * more_numbers - print 0 - 14 (10) times
  * Return: Always 0 (Success)
-*/
+ */
 
 void more_numbers(void)
 {
-		int num, row, count;
+	int num, row, count;
 
-		for (row = 0; row < 10; row++)
+	for (row = 0; row < 10; row++)
+	{
+		for (count = 0; count < 15; count++)
 		{
-			for (count = 0; count < 15; count++)
+			num = count;
+			if (count > 9)
 			{
-				num = count;
-				if (count > 9)
-				{
-					_putchar(1 + 48);
-					num = count + 48;
-				}
-				_putchar(num + 48);
+				_putchar(1 + 48);
+				num = count + 48;
 			}
+			_putchar(num + 48);
+		}
 		_putchar('\n');
+	}
 }
