ad02dad8806726b3e8f9a9e3b1866a17985658a8
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-14 08:57:25 -0700

    More_Functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 	int num, row, count;
 
-	for (row = 0; row < 11; row++)
+	for (row = 1; row <= 10; row++)
 	{
-		for (count = 0; count < 15; count++)
+		for (count = 0; count <= 14; count++)
 		{
 			num = count;
 			if (count > 9)
