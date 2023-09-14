05de59ff83be4bd6f904f5179bef595a74dd5e27
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-14 07:20:30 -0700

    More_Functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,13 @@
  *             only using _putchar twice
  *
  * Return: Always 0 (Success)
- */
+*/
 
 void print_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; i++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar(num);
 	}
