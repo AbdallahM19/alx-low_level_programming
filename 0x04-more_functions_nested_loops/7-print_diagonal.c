178fbc52d5598e2d39b5f9df61b28d4c342ee0c5
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-14 09:31:59 -0700

    More_Functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_diagonal - draws a diagonal line
+ * print_diagonal - print a diagonal line
  *
  * @n: is the number of times the \ character
  *     should be printed
@@ -9,15 +9,15 @@
 
 void print_diagonal(int n)
 {
-	int charn, space;
+	int chan, space;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (charn = 1; charn <= n; charn++)
+		for (chan = 1; chan <= n; ++chan)
 		{
-			for (space = 1; space <= charn; space++)
+			for (space = 1; space <= chan; ++space)
 				_putchar(' ');
 			_putchar(92);
 			_putchar('\n');
