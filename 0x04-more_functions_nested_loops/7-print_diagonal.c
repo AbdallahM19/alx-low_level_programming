30573458333b3e98abba6dda228425e0012c42be
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-14 09:22:07 -0700

    More_Functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,19 +5,22 @@
  *
  * @n: is the number of times the _ character
  *     should be printed
-*/
+ */
 
 void print_diagonal(int n)
 {
-	int charn;
+	int charn, space;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
 		for (charn = 1; charn <= n; charn++)
-			_putchar(' ');
-		_putchar(92);
-		_putchar('\n');
+		{
+			for (space = 1; space <= charn; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 }
