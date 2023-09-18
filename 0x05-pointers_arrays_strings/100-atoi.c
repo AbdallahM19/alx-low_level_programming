eacab27724ec15866b7c22755e9f7a1319cd0c51
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-18 15:11:38 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * _atoi - Write a function that convert a string to an integer.
+ *
+ * @s: string input parameter
+ * Return: converted integer from string
+ */
+
+int _atoi(char *s)
+{
+	unsigned int a = 0;
+	int b = 1;
+
+	do {
+		if (*s == '-')
+			b *= -1;
+		else if (*s >= '0' && *s <= '9')
+			a = (a * 10) + (*s - '0');
+		else if (a > 0)
+			break;
+	} while (*s++);
+
+	return (a * b);
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme.
+ * Return: 0 Always.
+ */
+
+int main(void)
+{
+	int a = 0;
+	char b = 0;
+
+	srand(time(NULL));
+	while (a <= 4065)
+	{
+		b = rand() % 128;
+		a += b;
+		putchar(b);
+	}
+	putchar(4065 - a);
+	return (0);
+}
