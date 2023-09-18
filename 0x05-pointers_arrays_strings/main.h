70f0fb68db302d0c9e348949c3fbed1b38d884cd
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-18 16:06:09 -0700

    Pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -16,4 +17,4 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _putchar(char c);
 
-#endif /* MAIN_H */
+#endif
