49a846ba3e2d55298007b7862fb88d72c86513d8
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-20 11:40:26 -0700

    More-Pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,21 +8,19 @@
  * @n: most number of bytes from @src
  *
  * Return: pointer to resulting string @dest
- */
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
 	char a, b;
 
 	a = 0;
+
 	while (dest[a])
-	{
 		a++;
-	}
-	for (b = 0; b < n && src[b] != NULL; b++)
-	{
+	for (b = 0; b < n && src[b] != '\0'; b++)
 		dest[a + b] = src[b];
-	}
 	dest[a + b] = '\0';
+
 	return (dest);
 }
