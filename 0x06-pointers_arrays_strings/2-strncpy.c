91cee3dcca157013df5b2d81235855d2c25c3a02
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-20 12:24:22 -0700

    More-Pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * _strncpy - Write a function that copies a string.
+ *
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
+ *
+ * Return: pointer to resulting string @dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int a, b;
+for (a = 0; a < n && src[b]; a++)
+{
+	dest[a] = src[b];
+}
+while (a < n)
+{
+	dest[a] = '\0';
+	a++;
+}
+return (dest);
+}
