826393eb39465e8f3a074bcb478693c3864c324c
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-20 11:36:47 -0700

    More-Pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * _strncat - Write a function that concatenates two strings.
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ * @n: most number of bytes from @src
+ *
+ * Return: pointer to resulting string @dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char a, b;
+
+	a = 0;
+	while (dest[a])
+	{
+		a++;
+	}
+	for (b = 0; b < n && src[b] != NULL; b++)
+	{
+		dest[a + b] = src[b];
+	}
+	dest[a + b] = '\0';
+	return (dest);
+}
