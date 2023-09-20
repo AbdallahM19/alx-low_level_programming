d0baf113404ea21ee5de67c9efddfedd820f7500
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-20 12:15:48 -0700

    More-Pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,18 +12,21 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int a, b;
+	int a;
+	int b;
 
 	a = 0;
-
 	while (dest[a] != '\0')
-		a++;
-	for (b = 0; b < n && src[b] != '\0'; b++)
 	{
-		dest[a] = src[b];
-		a++,
+		a++;
+	}
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+	dest[a] = src[b];
+	a++;
+	b++;
 	}
 	dest[a] = '\0';
-
 	return (dest);
 }
