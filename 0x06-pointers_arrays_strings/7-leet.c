b0d9a8217f9551026476e1f8f78b302b8b74329e
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-20 15:04:37 -0700

    More-Pointers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * leet - a function that encodes a string into 1337
+ *
+ * @a: string input
+ *
+ * Return: @a
+*/
+
+char *cap_string(char *a)
+{
+	int b, d;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (b = 0; a[b] != '\0'; b++)
+	{
+		for (d = 0; d < 10; d++)
+		{
+			if (a[b] == s1[d] && a[b] == s2[d])
+				return (a);
+		}
+	}
+}
+
