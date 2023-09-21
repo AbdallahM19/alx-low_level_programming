a2e0335e295589b13106ec9862c7e065fc463009
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-20 17:59:14 -0700

    More-Pointers

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,61 @@
 #include"main.h"
 
 /**
+ * isASCII - print 32 / 126.
+ * @a: int
+ * Return: 1 / 0
+ */
+
+int isASCII(int a)
+{
+	return (a >= 32 && a <= 126);
+}
+
+/**
+ * printf1 - printf1(char *b, int s1, int e1)
+ * @b: string
+ * @s1: start
+ * @e1: end
+ */
+
+void printf1(char *b, int s1, int e1)
+{
+	int d = 0;
+
+	while (d < 10)
+	{
+		if (d < e1)
+			printf("%02x", *(b + s1 + d));
+		else
+			printf("  ");
+		if (d % 2)
+			printf(" ");
+		d++;
+	}
+}
+
+/**
+ * printASCII -  prints a buffer.
+ * @b: string
+ * @s1: start
+ * @e1: end
+ */
+
+void printASCII(char *b, int s1, int e1)
+{
+	int c1, i = 0;
+
+	while (i < e1)
+	{
+		c1 = *(b + i + s1);
+		if (!isASCII(c1))
+			c1 = 46;
+		printf("%c", c1);
+		i++;
+	}
+}
+
+/**
  * print_buffer -  prints a buffer.
  * @b: buffer
  * @size: size
@@ -9,42 +64,19 @@
 
 void print_buffer(char *b, int size)
 {
-	int o, j, i;
+	int s1, e1;
 
-	o = 0;
-
-	if (size <= 0)
+	if (size > 0)
 	{
-		printf("\n");
-		return;
-	}
-	while (o < size)
-	{
-		j = size - o < 10 ? size - o : 10;
-		printf("%08x: ", o);
-		for (i = 0; i < 10; i++)
+		for (s1 = 0; s1 < size; s1 += 10)
 		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
-			else
-				printf("  ");
-			if (i % 2)
-			{
-				printf(" ");
-			}
+			e1 = (size - s1 < 10) ? size - s1 : 10;
+			printf("%08x: ", s1);
+			printf1(b, s1, e1);
+			printASCII(b, s1, e1);
+			printf("\n");
 		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + o + i);
-
-			if (c < 32 || c > 132)
-			{
-				c = '.';
-			}
-			printf("%c", c);
-		}
-		printf("\n");
-		o += 10;
 	}
+	else
+		printf("\n");
 }
-
