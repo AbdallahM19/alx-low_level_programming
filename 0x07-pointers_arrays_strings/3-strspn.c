a0c992bc061ee4e3202957410f830c3d148e91f7
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-25 16:57:25 -0700

    Pointers

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * _strspn - a function that gets the
+ *           length of a prexif substring
+ *
+ * @s: pointer
+ * @accept: substring
+ *
+ * Return: num of bytes in initial segment
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int a, b;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != s[a]; b++)
+		{
+			if (accept[b] == '\0')
+				return (b);
+		}
+	}
+	return (a);
+}
