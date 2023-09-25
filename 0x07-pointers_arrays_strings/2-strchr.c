c78ae9873f76af7b538e36bff422abc9a2347f67
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-25 16:46:51 -0700

    Pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,19 +6,19 @@
  * @s: pointer
  * @c: char
  *
- * Return: a pointer to the first occurrence of the character c in the string s
- *		null if not
- */
+ * Return: a pointer to the first occurrence of the character c in the string s,
+ *          or NULL if the character is not found
+*/
 
 char *_strchr(char *s, char c)
 {
 	int a;
 
-	for (a = 0; s[a] < '\0'; a++)
+	for (a = 0; s[a] >= '\0'; a++)
 	{
 		if (s[a] == c)
 		{
-			return (s);
+			return (s + a);
 		}
 	}
 	return ('\0');
