a36fd65487b38819f833e9ee16f88750d4bcc841
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-02 14:59:57 -0700

    argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: str
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", a);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
