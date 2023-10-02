abe8f1c0622d582be1b03b80057d591570471ee5
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-02 14:25:57 -0700

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ * @argc: int
+ * @argv: str
+ * Return: Always 0 (success).
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+}
