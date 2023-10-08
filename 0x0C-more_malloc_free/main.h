ba3754ac36536fa754b6b4f7a6fcf3c476fc2623
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-07 19:44:43 -0700

    calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,10 +12,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int put_s (char *s);
-int str_len(char *s);
-void error(void);
-int mul_strings(char *s1, char *s2, int *res);
-int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
