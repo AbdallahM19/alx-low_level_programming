9c55b13d79e2a77d281d390b4aa087a27269fac4
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-11 18:55:41 -0700

    function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,31 +12,26 @@
 
 int main(int argc, char **argv)
 {
-	int n1, n2;
-	int (*op_cal)(int, int);
+	int (*op_cal)(int, int), a, b;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 	op_cal = get_op_func(argv[2]);
 	if (!op_cal)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	if (!n2 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	printf("%d\n", op_cal(n1, n2));
+	printf("%d\n", op_cal(a, b));
 	return (0);
 }
