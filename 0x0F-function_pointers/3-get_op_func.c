bbe34a7b602cbfa89e7693b9f39b1fa99d7f763d
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-11 18:09:53 -0700

    function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,17 +16,12 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL},
+		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
 	for (i = 0; i < 5; i++)
-	{
 		if (s && s[0] == ops[i].op && !s[1])
-		{
 			return (ops[i].f);
-			i++;
-		}
-	}
 	return (NULL);
 }
