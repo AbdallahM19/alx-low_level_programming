895a989d264c141f2edfa442fc145d44f994e0af
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-11 17:58:27 -0700

    function

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+
+/**
+ * main - check the cal and give me the result
+ * @argc: The number of arguments
+ * @argv: An array of pointers
+ * Return: Always 0.
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int n1, n2;
+	int (*op_cal)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	op_cal = get_op_func(argv[2]);
+
+	if (!op_cal)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op_cal == '/' && n2 == 0) || (*op_cal == '%' && n2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", op_cal(n1, n2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum
+ * @a: The first num.
+ * @b: The second num.
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference
+ * @a: The first num.
+ * @b: The second num.
+ *
+ * Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product
+ * @a: The first num.
+ * @b: The second num.
+ *
+ * Return: product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: The first num
+ * @b: The second num
+ *
+ * Return: quotient
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division
+ * @a: The first num
+ * @b: The second num
+ *
+ * Return: remainder of the division
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
