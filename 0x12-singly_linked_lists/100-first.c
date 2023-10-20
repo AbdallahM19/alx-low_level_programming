af15f07fcaa1f36460e1350514d47fd32f6ad7e5
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-19 18:58:51 -0700

    lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,11 @@
 void before_main(void) __attribute__ ((constructor));
 
 /**
- * first - Prints the message before main
+ * before_main - Prints the message before main
  * Return: void
  */
 void before_main(void)
 {
- printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
