1c78f5658334d744ad57b0bb8b46ea400b997a6c
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-30 14:50:19 -0700

    bit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-#define USE "Usage: cp file_from file_to\n"
+#define USAGE "Usage: cp file_from file_to\n"
 #define ERROR_NOREAD "Error: Can't read from file %s\n"
 #define ERROR_NOWRITE "Error: Can't write to %s\n"
 #define ERROR_NOCLOSE "Error: Can't close fd %d\n"
-#define PERMISSION (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
  * main - Entry point,
@@ -14,18 +14,18 @@
  * Return: 0 on success,
  * exit codes as specified in the prompt on failure
  */
-int main(int ac, char *av[])
+int main(int ac, char **av)
 {
 	int from_fd = 0, to_fd = 0;
 	ssize_t a;
 	char buf[BUF_SIZE];
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, USE), exit(97);
+		dprintf(STDERR_FILENO, USAGE), exit(97);
 	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
 		dprintf(STDERR_FILENO, ERROR_NOREAD, av[1]), exit(98);
-	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSION);
+	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to_fd == -1)
 		dprintf(STDERR_FILENO, ERROR_NOWRITE, av[2]), exit(99);
 	while ((a = read(from_fd, buf, BUF_SIZE)) > 0)
