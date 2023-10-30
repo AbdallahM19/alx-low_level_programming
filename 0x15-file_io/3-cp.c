5de7470ed375fe4f6513777ac64cd4854242cea8
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-30 13:14:37 -0700

    bit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point, copies the content of one file to another.
- * @ac: Argument count.
- * @av: Argument vector.
- * Return: 0 on success, exit codes as specified in the prompt on failure.
+ * main - Entry point,
+ * copies the content of one file to another
+ * @ac: Argument count
+ * @av: Argument vector
+ * Return: 0 on success,
+ * exit codes as specified in the prompt on failure
  */
 int main(int ac, char *av[])
 {
@@ -17,7 +19,7 @@ int main(int ac, char *av[])
 	if (from_fd == -1)
 		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (to_fd == -1)
 		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 	while ((ch_r = read(from_fd, buffer, BUFFER_SIZE)) > 0)
