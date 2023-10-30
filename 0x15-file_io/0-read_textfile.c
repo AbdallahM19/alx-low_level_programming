11e83bef1449c60cb6c028c5485f61150781e6c1
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-30 08:47:25 -0700

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -27,7 +27,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	byte = read(fd, buffer, letters);
-	byte = write(STDOUT_FILENO, buffer, n);
+	byte = write(STDOUT_FILENO, buffer, byte);
 	close(fd);
 	free(buffer);
 	return (byte);
