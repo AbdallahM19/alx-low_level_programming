53f8d4eaee9587792f1566bd88dff7388af97541
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-30 14:36:58 -0700

    bit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,91 +1,42 @@
 #include "main.h"
 
-#define BUF_SIZE 1024
-
+#define USE "Usage: cp file_from file_to\n"
+#define ERROR_NOREAD "Error: can't read from file %s\n"
+#define ERROR_NOWRITE "Error: can't write to %s\n"
+#define ERROR_NOCLOSE "Error: can't close fd %s\n"
+#define PER (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 /**
- * print_error - print to STDERR_FILENO
- * @message: message to print
- */
-void print_error(const char *message)
-{
-	dprintf(STDERR_FILENO, "%s\n", message);
-}
-
-/**
- * copy_file - copy the content of one file to another
- * @file_from: source file descriptor
- * @file_to: destination file descriptor
- * @name_from: source file name
- * @name_to: destination file name
- * Return: 0 on success, exit codes on failure
- */
-int copy_file(int file_from, int file_to,
-const char *name_from, const char *name_to)
-{
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUF_SIZE];
-
-	while ((bytes_read = read(file_from, buffer, BUF_SIZE)) > 0)
-	{
-		bytes_written = write(file_to, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			print_error("Error: Can't write to file");
-			return (99);
-		}
-	}
-	if (bytes_read == -1)
-	{
-		print_error("Error: Can't read from file");
-		return (98);
-	}
-	if (close(file_from) == -1)
-	{
-		print_error("Error: Can't close file descriptor");
-		return (100);
-	}
-	if (close(file_to) == -1)
-	{
-		print_error("Error: Can't close file descriptor");
-		return (100);
-	}
-	return (0);
-}
-
-/**
- * main - Entry point, copies the content of one file to another
+ * main - Entry point,
+ * copies the content of one file to another
  * @ac: Argument count
  * @av: Argument vector
- * Return: 0 on success, exit codes as specified in the prompt on failure
+ * Return: 0 on success,
+ * exit codes as specified in the prompt on failure
  */
 int main(int ac, char *av[])
 {
-	int from_fd, to_fd;
-	struct stat st;
+	int from_fd = 0, to_fd = 0;
+	ssize_t a;
+	char buf[BUF_SIZE];
 
 	if (ac != 3)
-	{
-		print_error("Usage: cp file_from file_to");
-		return (97);
-	}
+		dprintf(STDERR_FILENO, USE), exit(97);
 	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
-	{
-		print_error("Error: Can't read from file");
-		return (98);
-	}
-	if (fstat(from_fd, &st) == -1)
-	{
-		print_error("Error: Can't get file status");
-		return (98);
-	}
-	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, st.st_mode);
+		dprintf(STDERR_FILENO, ERROR_NOREAD, av[1]), exit(98);
+	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PER);
 	if (to_fd == -1)
-	{
-		print_error("Error: Can't write to file");
-		return (99);
-	}
-	int result = copy_file(from_fd, to_fd, av[1], av[2]);
-
-	return (result);
+		dprintf(STDERR_FILENO, ERROR_NOWRITE, av[2]), exit(99);
+	while ((a = read(from_fd, buf, BUF_SIZE)) > 0)
+		if (write(to_fd, buf, a) != a)
+			dprintf(STDERR_FILENO, ERROR_NOWRITE, av[2]), exit(99);
+	if (a == -1)
+		dprintf(STDERR_FILENO, ERROR_NOWRITE, av[1]), exit(98);
+	from_fd = close(from_fd);
+	to_fd = close(to_fd);
+	if (from_fd)
+		dprintf(STDERR_FILENO, ERROR_NOCLOSE, from_fd), exit(100);
+	if (to_fd)
+		dprintf(STDERR_FILENO, ERROR_NOCLOSE, from_fd), exit(100);
+	return (0);
 }
