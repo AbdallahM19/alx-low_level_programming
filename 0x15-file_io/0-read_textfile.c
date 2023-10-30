314ea3518fa44a1ac41a44de63b0c766057e2041
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-30 08:31:53 -0700

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,30 +11,39 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char *buffer = (char *)malloc(letters);
+	char *buffer;
 
-	if (filename == NULL || letters == 0 || buffer == NULL)
+	if (filename == NULL || letters == 0)
 		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buffer);
 		return (0);
-	ssize_t n = read(fd, buffer, letters);
+	}
 
+	ssize_t n = read(fd, buffer, letters);
 	if (n == -1)
 	{
 		close(fd);
 		free(buffer);
 		return (0);
 	}
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, n);
 
-	if (bytes_written == -1 || bytes_written != n)
+	ssize_t bytes = write(STDOUT_FILENO, buffer, n);
+	if (bytes == -1 || bytes != n)
 	{
 		close(fd);
 		free(buffer);
 		return (0);
 	}
+
 	close(fd);
 	free(buffer);
-	return (bytes_written);
+	return (bytes);
 }
