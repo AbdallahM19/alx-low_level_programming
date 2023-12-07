367a1df0782c5f6a8f1ef2a5f6f1f2a8e88ec945
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-07 22:30:44 +0200

    doubly

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number in a doubly linked list
+ * @h: pointer
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint *h)
+{
+	for (size_t i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
