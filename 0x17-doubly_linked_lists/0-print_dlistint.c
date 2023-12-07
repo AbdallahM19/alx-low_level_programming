d7d50e2a12aa3f487a2403137d804c2bc40fd7b2
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-07 22:49:07 +0200

    doubly

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,15 +8,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	size_t i = 0;
 
-	if (!h)
-		return (0);
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		nodes++;
+		i++;
 	}
-	return (nodes);
+	return (i);
 }
