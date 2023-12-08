2858e0ca9a543b28bee182cfaab9aa4e48ff5406
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-08 04:07:08 +0200

    doubly

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -11,41 +11,48 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	char password[7], *codex;
-	int len = strlen(argv[1]), i, tmp;
+	int len = strlen(argv[1]), i = 0, tmp;
 
 	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
 	tmp = (len ^ 59) & 63;
 	password[0] = codex[tmp];
-
 	tmp = 0;
-	for (i = 0; i < len; i++)
+	while (i < len)
+	{
 		tmp += argv[1][i];
+		i++;
+	}
 	password[1] = codex[(tmp ^ 79) & 63];
-
 	tmp = 1;
-	for (i = 0; i < len; i++)
+	while (i < len)
+	{
 		tmp *= argv[1][i];
+		i++;
+	}
 	password[2] = codex[(tmp ^ 85) & 63];
-
 	tmp = 0;
-	for (i = 0; i < len; i++)
+	while (i < len)
 	{
 		if (argv[1][i] > tmp)
 			tmp = argv[1][i];
+		i++;
 	}
 	srand(tmp ^ 14);
 	password[3] = codex[rand() & 63];
-
 	tmp = 0;
-	for (i = 0; i < len; i++)
+	while (i < len)
+	{
 		tmp += (argv[1][i] * argv[1][i]);
+		i++;
+	}
 	password[4] = codex[(tmp ^ 239) & 63];
-
-	for (i = 0; i < argv[1][0]; i++)
+	while (i < argv[1][0])
+	{
 		tmp = rand();
+		i++;
+	}
 	password[5] = codex[(tmp ^ 229) & 63];
-
 	password[6] = '\0';
 	printf("%s", password);
 	return (0);
