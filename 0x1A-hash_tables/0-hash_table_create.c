cf536603f52ed035077145c82af8d3413e12a2fa
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-22 15:39:45 +0200

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,6 @@
 /**
  * hash_table_create - Creates a hash table.
  * @size: The size of the array.
- *
  * Return: A pointer the newly created hash table,NULL if an error occurs.
  */
 hash_table_t *hash_table_create(unsigned long int size)
