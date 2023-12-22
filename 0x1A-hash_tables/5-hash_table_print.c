02c945d581eb1e01a053f5c6a22e909dc76cc87d
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-22 16:27:43 +0200

    hash

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: The hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int i = 0;
+    hash_node_t *node;
+    int first = 1;
+
+    if (ht == NULL)
+        return;
+    printf("{");
+    while (i < ht->size)
+    {
+        node = ht->array[i];
+        while (node != NULL)
+        {
+            if (!first)
+                printf(", ");
+            printf("'%s': '%s'", node->key, node->value);
+            first = 0;
+            node = node->next;
+        }
+        i++;
+    }
+    printf("}\n");
+}
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table to be deleted
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int i = 0;
+    hash_node_t *node, *current_node;
+
+    if (ht == NULL)
+        return;
+    while (i < ht->size)
+    {
+        node = ht->array[i];
+        while (node != NULL)
+        {
+            current_node = node->next;
+            free(node->key);
+            free(node->value);
+            free(node);
+            node = current_node;
+        }
+        i++;
+    }
+    free(ht->array);
+    free(ht);
+}
