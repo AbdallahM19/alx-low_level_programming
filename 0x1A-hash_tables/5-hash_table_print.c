75d2f149eb3d75e329dd6a33f02767a55581307f
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-22 16:29:14 +0200

    hash

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,25 +6,26 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i = 0;
-    hash_node_t *node;
-    int first = 1;
+	unsigned long int i = 0;
+	hash_node_t *node;
+	int first = 1;
 
-    if (ht == NULL)
-        return;
-    printf("{");
-    while (i < ht->size)
-    {
-        node = ht->array[i];
-        while (node != NULL)
-        {
-            if (!first)
-                printf(", ");
-            printf("'%s': '%s'", node->key, node->value);
-            first = 0;
-            node = node->next;
-        }
-        i++;
-    }
-    printf("}\n");
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (i < ht->size)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+		i++;
+	}
+	printf("}\n");
 }
+
