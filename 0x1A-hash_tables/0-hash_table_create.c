5e959ac15ed9dee777673e949d04672b94f785d2
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-22 15:18:58 +0200

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,25 +8,18 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	unsigned long int i = 0;
+	hash_table_t *hash_tables;
 
 	if (size == 0)
 		return (NULL);
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+
+	hash_tables = malloc(sizeof(hash_table_t));
+	if (!hash_tables)
 		return (NULL);
-	hash_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (hash_table->array == NULL)
-	{
-		free(hash_table);
+
+	hash_tables->size = size;
+	hash_tables->array = calloc((size_t)hash_tables->size, sizeof(hash_node_t *));
+	if (hash_tables->array == NULL)
 		return (NULL);
-	}
-	while (i < size)
-	{
-		hash_table->array[i] = NULL;
-		i++;
-	}
-	hash_table->size = size;
-	return (hash_table);
+	return (hash_tables);
 }
