52b9475ee6b0c9b8db132b1e55a119911b8334d1
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-12-22 16:32:17 +0200

    hash

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,24 +6,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i = 0;
-    hash_node_t *node, *current_node;
+	unsigned long int i = 0;
+	hash_node_t *node, *current_node;
 
-    if (ht == NULL)
-        return;
-    while (i < ht->size)
-    {
-        node = ht->array[i];
-        while (node != NULL)
-        {
-            current_node = node->next;
-            free(node->key);
-            free(node->value);
-            free(node);
-            node = current_node;
-        }
-        i++;
-    }
-    free(ht->array);
-    free(ht);
+	if (ht == NULL)
+		return;
+	while (i < ht->size)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			current_node = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = current_node;
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
 }
+
